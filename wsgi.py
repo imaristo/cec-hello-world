d8c389a85e28ee00874cd2a29fe184dbe8b463ea
imaristo <ilkka.maristo@kolumbus.fi>
2018-02-28 19:06:18 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,12 @@
+import datetime
 import socket
 from flask import Flask
+
+now = datetime.datetime.now()                                                                                                                      
+                                                                                                                                                       
+f= open("/mnt/hello_log.txt","a+")                                                                                                                 
+f.write ("%r,%r\n" % (socket.gethostname(), now.strftime("%Y-%m-%d %H:%M:%S")))                                                                    
+f.close()  
 
 application = Flask(__name__)
 
